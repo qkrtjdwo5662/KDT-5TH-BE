fab1e73ab12b2f235f111879866bfeb2ba84fd01
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-03-24 09:34:24 +0900

    Update dbConnect.js

--- 03_express/controllers/dbConnect.js
+++ 03_express/controllers/dbConnect.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql'); // mysql
+// const mysql = require('mysql'); // mysql
 
-// connection 생성 및 mysql 정보
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  port: '3306',
-  database: process.env.MYSQL_DB,
-});
-connection.connect();
+// // connection 생성 및 mysql 정보
+// const connection = mysql.createConnection({
+//   host: '127.0.0.1',
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   port: '3306',
+//   database: process.env.MYSQL_DB,
+// });
+// connection.connect();
 
-module.exports = connection;
+// module.exports = connection;
